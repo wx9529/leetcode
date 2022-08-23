3ab9d7a9b22dd575299fa49b6575af8e36f88909
wx9529 <xinwang213z@gmail.com>
2022-08-22 21:25:13 -0700

    q11

---
+++ arr_q11.js
@@ -0,0 +1,14 @@
+// https://github.com/youngyangyang04/leetcode-master/blob/master/problems/0027.%E7%A7%BB%E9%99%A4%E5%85%83%E7%B4%A0.md
+
+const arr = [0, 1, 2, 2, 3, 0, 4, 2], val = 2;
+
+const removeElement = (nums, val) => {
+  let k = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] != val) {
+      nums[k++] = nums[i];
+    }
+  }
+  return k;
+}
+console.log(removeElement(arr, val));
